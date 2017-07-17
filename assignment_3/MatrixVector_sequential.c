27aacfef0d8e1b76446f5b668a7a8fa26d5be984
ayolas <pankaja.patki@gmail.com>
2017-07-17 12:37:25 -0500

    Uploaded latest Matrixvector sequential

--- assignment_3/MatrixVector_sequential.c
+++ assignment_3/MatrixVector_sequential.c
@@ -1,4 +1,5 @@
-
+/* HW3 - by John M and Pankaja A. */
+/*Implementing matrix vector multiplication sequentially*/
 
 #include <stdio.h>
 #include <errno.h>
@@ -7,30 +8,14 @@
 
 #define N 100
 
-int main(int argc, char *argv[]) {
-    
-    if ( argc != 3 ) /* argc should be 3 for correct execution */
-    {
-        /* We print argv[0] assuming it is the program name */
-        printf( "usage: %s file1, file2, noOfThreads", argv[0] );
-    }
-    else
-    {
-        doMult(argv[1], argv[2]);
-    }
-    return 0;
-}
-
-
-
-int doMult(char file1[], char file2[])
+int Multiply(char file1[], char file2[])
 {
     
     FILE * pFile1;
     FILE * pFile2;
     
     int R1;
-    int C1,C2,sum;
+    int C1,sum;
     int a,b,c,d;
     
     //double Matrix1[N][N];
@@ -52,21 +37,21 @@ int doMult(char file1[], char file2[])
     if (pFile1!=NULL) {
         fscanf(pFile1, "%d", &R1);
         fscanf(pFile1, "%d\n", &C1);
-        printf("Rows %d, Columns %d", R1,C1);
+        //printf("Rows %d, Columns %d \n", R1,C1);
         for (a = 0; a < R1; a++)
         {
             for (b = 0; b < C1; b++)
             {
-               //fscanf(pFile1,"%lf", &Matrix1[a][b]);
+                //fscanf(pFile1,"%lf", &Matrix1[a][b]);
                 fscanf(pFile1,"%d", &Matrix1[a][b]);
-
+                
             }
         }
     }
     fclose (pFile1);
     printf("\n");
-  //---------------------------
-    printf("Printing Matrix: \n");
+    //---------------------------
+    /*printf("Printing Matrix: \n");
     for (a = 0; a < R1; a++)
     {
         for (b = 0; b < C1; b++)
@@ -74,11 +59,12 @@ int doMult(char file1[], char file2[])
             //printf("%lf ", Matrix1[a][b]);
             printf("%d ", Matrix1[a][b]);
         }
-         printf("\n");
+        printf("\n");
     }
-      printf("\n");
-  //---------------------------
-   
+    printf("\n");*/
+    //---------------------------
+    
+    //Here i am assuming that the vector has the same number of elements as the coloumns of matrix1.
     
     pFile2 = fopen (file2,"r");
     if (pFile2 == 0)
@@ -89,58 +75,75 @@ int doMult(char file1[], char file2[])
     
     if (pFile2!=NULL)
     {
-        
         for (c = 0; c < C1; c++)
         {
-           //fscanf(pFile2,"%lf", &Vector1[c]);
+            //fscanf(pFile2,"%lf", &Vector1[c]);
             fscanf(pFile2,"%d", &Vector1[c]);
-            
         }
     }
     
     fclose (pFile2);
     
     //---------------------------
-    printf("Printing Vector: \n");
+    /*printf("Printing Vector: \n");
     for (a = 0; a < C1; a++)
     {
-         //printf("%lf ", Vector1[a]);
+        //printf("%lf ", Vector1[a]);
         printf("%d ", Vector1[a]);
-
+        
     }
-     printf("\n");
+    printf("\n");*/
     //---------------------------
     
-       for(a=0; a < R1; a++)
+    
+    for(a=0; a < R1; a++)
+    {
+        int sum =0;
+        for(b=0;b < C1; b++)
         {
-            int sum =0;
-            for(b=0;b < C1; b++)
-            {
-                sum = sum + Matrix1[a][b] * Vector1[b] ;
-            }
-                Result[a] = sum;
-            
-       }
-            
+            sum = sum + Matrix1[a][b] * Vector1[b] ;
+        }
+        Result[a] = sum;
+        
+    }
+    
     
     FILE *fptr;
     fptr = fopen("Result.txt", "w");
-     
+    
     for(a=0; a < R1; a++)
     {
         fprintf(fptr,"%d\t", Result[a]);
-          //printf("%d\t", Result[a][0]);
+        //printf("%d\t", Result[a]);
         
         fprintf(fptr,"\n");
         //printf("\n");
     }
     
-    printf("Multiplication Results are in Result.txt\n");
+    
+    printf("\nMultiplication Results are in Result.txt\n");
     
     fclose(fptr);
     
     return 0;
 }
+
+
+int main(int argc, char *argv[]) {
+    
+    if ( argc != 3 ) /* argc should be 3 for correct execution */
+    {
+        /* We print argv[0] assuming it is the program name */
+        printf( "usage: %s file1, file2", argv[0] );
+    }
+    else
+    {
+        Multiply(argv[1], argv[2]);
+    }
+    return 0;
+}
+
+
 
 
 
