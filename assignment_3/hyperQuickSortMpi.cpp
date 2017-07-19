01695e5ac856fa3cddc91ea6ee194377c584ed13
John Martinez <johnjmartinez@utexas.edu>
2017-07-19 04:22:45 -0500

    Algorithm works ... stuck on gathering data correctly

--- assignment_3/hyperQuickSortMpi.cpp
+++ assignment_3/hyperQuickSortMpi.cpp
@@ -7,36 +7,24 @@
 #include <mpi.h>
 
 #define MASTER 0 
-
-#define debug2
 using namespace std;
-/*
-    PARALLEL HYPERQUICKSORT (HQsort)
-    Read an array A from a file named input.txt.  
-    Divide the input array into roughly equal sized chunks n/p chunks (total rows: n, total processes: p) 
-    Compute an array D such that it is sorted in ascending order using Parallel HyperQuickSort. 
-    After HyperQuickSort array D is split among p procs s.t. higher numbered proc has higher entries. 
-    Write the output in a file called output.txt
-    
-    ALGORITHM:
-    for i = d down to 1
-        for each i-cube:
-            Root of the i-cube broadcasts its median to all in the i-cube, to serve as pivot
-            considering the two (i-1)-subcubes of this i-cube, each pair of partners in (i-1)-subcubes exchanges data:
-                low-numbered PE gives its partner its data larger than pivot
-                high-numbered PE gives its partner its data smaller than pivot
-*/     
 
-int getMedian( const vector<int>& V ) { //DON'T CHANGE THE DAMN VECTOR!
+int getMedian( const vector<int>& V ) { 
     const auto iter1 = V.begin() + V.size() / 2 - 1;
     const auto iter2 = V.begin() + V.size() / 2;
-    //nth_element(V.begin(), iter1 , V.end());  // VECTOR already sorted ... redundant nth_elem() jumbles it
-    //nth_element(V.begin(), iter2 , V.end()); 
     auto median = (V.size() % 2 == 0) ? (*iter1 + *iter2) / 2 : *iter2;
     return median;
 }
 
 void HQsort (vector<int>& V, int& size) {
+/* PARALLEL HYPERQUICKSORT (HQsort)    
+    for i = d down to 1
+        for each i-cube:
+            Root of the i-cube broadcasts its median to all in the i-cube, to serve as pivot
+            considering two (i-1)-subcubes of i-cube, each pair of partners in (i-1)-subcubes exchanges data:
+                low-numbered PE gives its partner its data larger than pivot
+                high-numbered PE gives its partner its data smaller than pivot
+*/     
     int pivot, incoming_size;
     vector<int> incoming_V;
 
@@ -48,21 +36,16 @@ void HQsort (vector<int>& V, int& size) {
     int dimensions = log(num_procs)/log(2);
     int level = num_procs;
     int partner_id;
+    int root = num_procs/2;
 
     for (int d = dimensions ; d > 0; d--) { //TRANSVERSE I-CUBES
-    
-        level = level >> 1;
-    
-        if ( curr_id == level ) {
-            pivot = getMedian(V);
-#ifdef debug2
-            cout << "Pivot " << pivot << endl;
-            cout << "Dimns " << d << endl;
-            cout << "Level " << level << endl;
-#endif
-        }
+        MPI::COMM_WORLD.Barrier();
         
-        MPI::COMM_WORLD.Bcast( &pivot, 1, MPI::INT, MASTER ); // Bcast pivot
+        level = level >> 1;
+        if ( curr_id == root ) 
+            pivot = getMedian(V);
+       
+        MPI::COMM_WORLD.Bcast( &pivot, 1, MPI::INT, root ); // Bcast pivot
         
         const auto iter = stable_partition( V.begin(), V.end(), [pivot](int n){ return n < pivot; } ); // ninja lambda       
         vector<int> lower(V.begin(), iter); // less than pivot 
@@ -70,17 +53,15 @@ void HQsort (vector<int>& V, int& size) {
         
         if( (curr_id & level) == 0 ) {  // LOWER SUBCUBE
             partner_id = curr_id + level;
-            // https://www.open-mpi.org/doc/v1.10/man3/MPI_SendrecV.3.php
             // exchange sizes first
             int chunk_size = upper.size();
             MPI::COMM_WORLD.Sendrecv(&chunk_size, 1, MPI::INT, partner_id, d,
                                      &incoming_size, 1, MPI::INT, partner_id, d);
-            // exchange upper chunks
+            // send upper chunk
             incoming_V.assign(incoming_size, 0);
             MPI::COMM_WORLD.Sendrecv(&upper.front(), upper.size(), MPI::INT, partner_id, d,
                                      &incoming_V.front(), incoming_size, MPI::INT, partner_id, d);
             // merge lower + incoming   
-            //V = lower; V.insert(V.end(), incoming_V.begin(), incoming_V.end()); // dumb concat doesn't work 
             V.assign(lower.size()+incoming_V.size(), 0);
             merge(lower.begin(), lower.end(), incoming_V.begin(), incoming_V.end(), V.begin());                 
         }
@@ -90,24 +71,26 @@ void HQsort (vector<int>& V, int& size) {
             int chunk_size = lower.size();
             MPI::COMM_WORLD.Sendrecv(&chunk_size, 1, MPI::INT, partner_id, d,
                                      &incoming_size, 1, MPI::INT, partner_id, d);
-            // exchange lower chunks
+            // send lower chunk
             incoming_V.assign(incoming_size, 0);
             MPI::COMM_WORLD.Sendrecv(&lower.front(), lower.size(), MPI::INT, partner_id, d,
                                      &incoming_V.front(), incoming_size, MPI::INT, partner_id, d);    
             // merge incoming + upper        
-            //V = incoming_V; V.insert(V.end(), upper.begin(), upper.end()); // dumb concat doesn't work
             V.assign(incoming_V.size()+upper.size(), 0);
             merge(incoming_V.begin(), incoming_V.end(), upper.begin(), upper.end(), V.begin());                 
         }
-    } 
+        
+        size = V.size();
+        root = root >> 1;
+    } //END FOR 
 }
-
 
 // MAIN
 int main(int argc, char *argv[]) {
     
-    vector<int> A,  chunkVect;
-    int chunkSize;
+    vector<int> chunkVect;
+    vector<int> A, displacement, count, D; // MASTER use only
+    int chunkSize, local;
     
     MPI::Init( argc, argv );
 
@@ -118,50 +101,44 @@ int main(int argc, char *argv[]) {
         cout <<" Usage: %s <input.txt>" << argv[0] << endl;
         return -1;
     }
-    
+        
     //MASTER: READ ARRAY AND SCATTER EVENLY AMONG PROCS
     if ( id == MASTER ) { 
-
         ifstream inFile( argv[1]) ;
         istream_iterator<int> start(inFile), end;
         A.assign(start, end);
         cout << "Read " << A.size() << " numbers" << endl;
-
-#ifdef debug1
-        cout << "numbers read in:\n";
-        copy(A.begin(), A.end(), ostream_iterator<int>(cout, " "));
-        cout << endl;
-#endif    
         chunkSize = A.size()/num_procs;
+
+        D.assign(A.size(), 0);
+        displacement.assign(num_procs, 0);
+        count.assign(num_procs, 0);
     }
     
-    // https://www.open-mpi.org/doc/v1.10/man3/MPI_Bcast.3.php
-    MPI::COMM_WORLD.Bcast( &chunkSize, 1, MPI::INT, MASTER ); // only MASTER can Bcast (id=0)
+    MPI::COMM_WORLD.Bcast( &chunkSize, 1, MPI::INT, MASTER ); // Bcast chunk size
     chunkVect.assign(chunkSize, 0); // init vector size
-#ifdef debug1
-    cout << id << " : " << chunkSize << endl;
-#endif    
    
-    // https://www.open-mpi.org/doc/v1.10/man3/MPI_Scatter.3.php    
-    MPI::COMM_WORLD.Scatter( &A[(id)*(chunkSize)], chunkSize, MPI::INT, // sending info -- only MASTER can Scatter
+    MPI::COMM_WORLD.Scatter( &A[(id)*(chunkSize)], chunkSize, MPI::INT, // split A per proc id
                              &chunkVect.front(), chunkSize, MPI::INT,   // space for procs to receive
                              MASTER );    
-#ifdef debug1
-    cout << id << " : ";
-    for(auto& x:chunkVect) cout << x << ' ';
-    cout << endl;
-#endif    
                                    
     HQsort(chunkVect, chunkSize);
-   
-#ifdef debug2
-    cout << id << " : ";
-    for(auto& x:chunkVect) cout << x << ' ';
-    cout << endl;
-#endif    
-   
+    
+    MPI::COMM_WORLD.Scan( &chunkSize, &local, 1, MPI::INT, MPI::SUM);
+    
+    MPI::COMM_WORLD.Gather(&local, 1, MPI::INT, &displacement[id+1], 1, MPI::INT, MASTER);
+    MPI::COMM_WORLD.Gather(&chunkSize, 1, MPI::INT, &count[id], 1, MPI::INT, MASTER);
+    
+    MPI::COMM_WORLD.Gatherv(&chunkVect, chunkSize, MPI::INT, &D.front(), &count[id], &displacement[id], MPI::INT, MASTER);
+
+    if ( id == MASTER ) { 
+        for(auto& x:displacement) cout << x << '\t'; cout << endl;    
+        for(auto& x:count) cout << x << '\t'; cout << endl;    
+
+        ofstream outFile("./output.txt");
+        for (const auto &e : D) outFile << e << " ";
+    }
+    
     MPI:: Finalize();
-    return 0;
-  
-   
+    return 0;   
 }
