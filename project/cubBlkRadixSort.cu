f46207bd586ee4f278a081fea49a842f21186cba
John Martinez <johnjmartinez@gmail.com>
2017-08-01 02:25:37 -0500

    Got ALL cub sorts to work correctly

--- project/cubBlkRadixSort.cu
+++ project/cubBlkRadixSort.cu
@@ -39,11 +39,8 @@ __global__ void cubBlkSort(T *d_in, int *d_out) {
 template <typename T>
 void cubDevSort (int COUNT, T *d_in, T *d_out) {
     
-    int beginBit = 0, endBit = 8*sizeof(T); // Bit subrange [beginBit, endBit) of differentiating elem bits
-    
-    DoubleBuffer<int> dElems;
-    checkCudaErrors(cudaMalloc((void**)&dElems.d_buffers[0], COUNT*sizeof(T)));
-    checkCudaErrors(cudaMalloc((void**)&dElems.d_buffers[1], COUNT*sizeof(T)));
+    int beginBit = 0, endBit = 32; // Bit subrange [beginBit, endBit) for differentiating elem bits
+    DoubleBuffer<T> dElems(d_in, d_in+COUNT);
     
     void *dTmp = NULL;
     size_t dTmpSize = 0;
@@ -52,10 +49,9 @@ void cubDevSort (int COUNT, T *d_in, T *d_out) {
     checkCudaErrors(cudaMalloc (&dTmp, dTmpSize)); // Allocate temp storage
     
     // Run sorting operation
-    cudaMemcpy(dElems.d_buffers[dElems.selector], d_in, sizeof(T) * COUNT, cudaMemcpyDeviceToDevice);
     checkCudaErrors(DeviceRadixSort::SortKeys(dTmp, dTmpSize, dElems, COUNT, beginBit, endBit));
      
-    cudaMemcpy(d_out, dElems.Current(), sizeof(T) * COUNT, cudaMemcpyDeviceToDevice);
+    cudaMemcpy(d_out, dElems.Current(), sizeof(T)*COUNT, cudaMemcpyDeviceToDevice);
     checkCudaErrors(cudaFree(dTmp)); // Release temp storage
 }
 
@@ -87,8 +83,8 @@ int main(int argc, char** argv) {
     unsigned int memAlloc = DATASIZE * sizeof(int);
     h_ary = (int*)malloc(memAlloc); // allocatings_output memory for Hosts[]
     h_ref = (int*)malloc(memAlloc); // allocatings_output memory for Hosts[]
-    cudaMalloc((void**)&d_1, memAlloc+2); // allocating memory for Dev[] + paddinng
-    cudaMalloc((void**)&d_2, memAlloc+2); // allocating memory for Dev[] + paddinng
+    cudaMalloc((void**)&d_1, memAlloc*8); // allocating memory for Dev[] + paddinng
+    cudaMalloc((void**)&d_2, memAlloc*8); // allocating memory for Dev[] + paddinng
 
     printf("Input array size : %d\n", DATASIZE);
     srand(time(NULL));
