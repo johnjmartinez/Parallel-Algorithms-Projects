d95cc73dc1164fe11ab5af6f3ea23542d7548da2
John Martinez <johnjmartinez@gmail.com>
2017-08-02 02:54:39 -0500

    adding laptop results + optimized bricksort3 for it

--- project/compileIt.sh
+++ project/compileIt.sh
@@ -10,7 +10,7 @@ nvcc $_cu_ -O3 \
     -I cuda_common/inc/ -I cub/ -I ./\
     -lineinfo -l cuda -use_fast_math \
     -std=c++11 --expt-extended-lambda \
-    -arch=compute_61 -code=sm_61 \
+    -arch=compute_30 -code=sm_30 \
     -o ${_cu_%.cu}_ 
 
 #    -D_MWAITXINTRIN_H_INCLUDED -D_FORCE_INLINES \
