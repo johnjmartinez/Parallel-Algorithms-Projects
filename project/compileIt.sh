aaf72d0cb7810e19711a64bd7ca03ee062623700
John Martinez <johnjmartinez@gmail.com>
2017-08-02 02:14:49 -0500

    not much progress ... calling it a night

--- project/compileIt.sh
+++ project/compileIt.sh
@@ -7,10 +7,10 @@
 set -x
 _cu_=$1
 nvcc $_cu_ -O3 \
-    -I cuda_common/inc/ -I cub/ \
-    -lineinfo -l cuda\
-    -std=c++11 --expt-extended-lambda\
+    -I cuda_common/inc/ -I cub/ -I ./\
+    -lineinfo -l cuda -use_fast_math \
+    -std=c++11 --expt-extended-lambda \
     -arch=compute_61 -code=sm_61 \
-    -o ${_cu_%.cu}_ -use_fast_math 
+    -o ${_cu_%.cu}_ 
 
 #    -D_MWAITXINTRIN_H_INCLUDED -D_FORCE_INLINES \
